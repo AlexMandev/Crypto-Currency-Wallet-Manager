2ccd00ada9f23291dd76c2ceff540b386c30ad6f
AlexMandev <alexmandev2004@gmail.com>
2025-01-20 10:29:33 +0200

    implement User class

--- src/bg/sofia/uni/fmi/mjt/crypto/wallet/user/User.java
+++ src/bg/sofia/uni/fmi/mjt/crypto/wallet/user/User.java
@@ -1,5 +1,43 @@
 package bg.sofia.uni.fmi.mjt.crypto.wallet.user;
 
+import bg.sofia.uni.fmi.mjt.crypto.wallet.algorithm.HashAlgorithm;
+
+import java.util.Objects;
+
 public class User {
 
+    private final String username;
+    private final String hashedPassword;
+    private final Wallet wallet;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.hashedPassword = HashAlgorithm.hashPassword(password);
+        this.wallet = new Wallet();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean matchesPassword(String password) {
+        return HashAlgorithm.hashPassword(password).equals(hashedPassword);
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(username);
+    }
 }
---
+++ src/bg/sofia/uni/fmi/mjt/crypto/wallet/user/Wallet.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.crypto.wallet.user;
+
+import bg.sofia.uni.fmi.mjt.crypto.wallet.model.Asset;
+
+public class Wallet {
+
+}
