616aea263aad12a5bb3418094e6c9134acaf5bbd
AlexMandev <alexmandev2004@gmail.com>
2025-01-20 08:34:36 +0200

    implement hashing algorithm

---
+++ src/bg/sofia/uni/fmi/mjt/crypto/wallet/algorithm/HashAlgorithm.java
@@ -0,0 +1,44 @@
+package bg.sofia.uni.fmi.mjt.crypto.wallet.algorithm;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashAlgorithm {
+    private static final int HASH_BYTE = 0xff;
+    private static final String HASHING_ALGORITHM = "SHA-256";
+    private static final char LEADING_ZERO = '0';
+
+    public static String hashPassword(String password) {
+        String hashedPassword;
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(HASHING_ALGORITHM);
+            byte[] hashedBytes = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
+
+            hashedPassword = buildHexString(hashedBytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error occurred while hashing password" , e);
+        }
+
+        return hashedPassword;
+    }
+
+    private static String buildHexString(byte[] hashedBytes) {
+        StringBuilder hashedPassword = new StringBuilder();
+        for (byte b : hashedBytes) {
+            String hex = Integer.toHexString(b & HASH_BYTE);
+            if (hex.length() == 1) {
+                hashedPassword.append(LEADING_ZERO);
+            }
+            hashedPassword.append(hex);
+        }
+        return hashedPassword.toString();
+    }
+
+    public static void main(String[] args) {
+        String password = "sherwoodalex2004";
+
+        System.out.println(hashPassword(password));
+        System.out.println(hashPassword(password));
+    }
+}
