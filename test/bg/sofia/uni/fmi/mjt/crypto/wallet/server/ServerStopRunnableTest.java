849829b5cd215e1782491631f79542b7464d55cd
AlexMandev <alexmandev2004@gmail.com>
2025-02-10 12:06:35 +0200

    add tests

--- test/bg/sofia/uni/fmi/mjt/crypto/wallet/server/ServerStopRunnableTest.java
+++ test/bg/sofia/uni/fmi/mjt/crypto/wallet/server/ServerStopRunnableTest.java
@@ -12,7 +12,6 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class ServerStopRunnableTest {
 
--- test/bg/sofia/uni/fmi/mjt/crypto/wallet/user/UserTest.java
+++ test/bg/sofia/uni/fmi/mjt/crypto/wallet/user/UserTest.java
@@ -1,13 +1,10 @@
 package bg.sofia.uni.fmi.mjt.crypto.wallet.user;
-
-import bg.sofia.uni.fmi.mjt.crypto.wallet.algorithm.HashAlgorithm;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
